c121915b480d09169e528d4be877815561ec2e1e
Diallo <Diallo@Diallo-2.local>
2025-01-26 17:55:40 -0500

    H&S classwork

---
+++ lab08.py
@@ -0,0 +1,10 @@
+def print_lyrics():
+	print ("i'm a programmer, and i'm okay.")
+	print ("i code all night, and i code all day.")
+def repaeat_lyrics():
+	print_lyrics()
+	print_lyrics()
+def main():
+	repaeat_lyrics()
+if __name__== "__main__":
+	main()
